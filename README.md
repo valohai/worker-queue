c5f31db1307aa040ae23b09077e18875d2e4f37e
Otso Rasimus <orasimus@gmail.com>
2021-06-16 16:59:06 +0300

    Add a setup script for easy worker queue instance setup

--- README.md
+++ README.md
@@ -13,3 +13,13 @@ The Docker image built is based on the official Ubuntu 20.04 image and contains:
 Build: `./build.sh`
 
 Publish: `./publish.sh`
+
+## Installation
+
+> :warning: The worker queue host machine needs to have port 80 open to the internet and port 63790 open to Valohai (roi) and workers (peon)!
+
+Installing the worker queue on a machine is as easy as running the setup script: `sudo ./setup.sh REDIS_PASSWORD VQUEUE_ADDRESS`
+
+This installs Docker and creates a systemd service that runs the worker queue with the given parameters.
+
+VQUEUE_ADDRESS is the `xxx.vqueue.net` address configured in Valohai's AWS Route 53 and REDIS_PASSWORD is an agreed-upon password for Redis.
---
+++ host/setup.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+if test "$#" -ne 2; then
+    >&2 echo "Invalid amount of parameters, QUEUE_ADDRESS and REDIS_PASSWORD required"
+    exit 1
+fi
+
+QUEUE_ADDRESS=$1
+REDIS_PASSWORD=$2
+DOCKER_IMAGE=valohai/worker-queue:latest
+
+# Upgrade system
+
+apt-get update
+apt-get upgrade -y
+
+# Install Docker
+
+if test ! -f /usr/share/keyrings/docker-archive-keyring.gpg; then
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+fi
+
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# Pull worker queue image
+
+docker pull $DOCKER_IMAGE
+
+# Setup a systemd service for automatic restarts
+
+cat << EOM > /etc/systemd/system/worker-queue.service
+[Unit]
+Description=Valohai Worker Queue
+After=docker.service
+Requires=docker.service
+
+[Service]
+Restart=always
+ExecStart=/usr/bin/docker run --rm \
+                              --name %n \
+                              --network host \
+                              $DOCKER_IMAGE $QUEUE_ADDRESS $REDIS_PASSWORD
+ExecStop=/usr/bin/docker stop %n
+
+[Install]
+WantedBy=multi-user.target
+EOM
+
+systemctl enable worker-queue
+systemctl start worker-queue
