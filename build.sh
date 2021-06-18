bbd1ab494c8ca3a30489a4a2cc197046ffdc7777
Otso Rasimus <orasimus@gmail.com>
2021-06-18 13:30:20 +0300

    Add the acmetool binary to the repository

--- build.sh
+++ build.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-if test ! -f docker/acmetool/acmetool-v2-beta-linux; then
-    wget https://valohai-cfg.s3.amazonaws.com/pub/acmetool-v2-beta-linux --directory-prefix=docker/acmetool/
-fi
-
-chmod +x docker/acmetool/acmetool-v2-beta-linux
-
 docker build -t valohai/worker-queue:latest docker
