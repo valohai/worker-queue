eb54357bc167597fd01c571e73ce945eccd019fc
Aarni Koskela <akx@iki.fi>
2021-06-18 08:45:57 +0300

    Fix shellcheck SC2027: The surrounding quotes actually unquote this. Remove or escape them.

--- docker/run_worker_queue.sh
+++ docker/run_worker_queue.sh
@@ -12,7 +12,7 @@ if test ! -d /var/lib/acme/live/"$QUEUE_ADDRESS"; then
     acmetool want "$QUEUE_ADDRESS"
 fi
 
-sed -i "s|# requirepass foobared|requirepass "$REDIS_PASSWORD"|" /etc/redis/redis.conf
-sed -i "s|FILL_ADDRESS|"$QUEUE_ADDRESS"|" /etc/redis/redis.conf
+sed -i "s|# requirepass foobared|requirepass $REDIS_PASSWORD|" /etc/redis/redis.conf
+sed -i "s|FILL_ADDRESS|$QUEUE_ADDRESS|" /etc/redis/redis.conf
 
 exec redis-server /etc/redis/redis.conf
