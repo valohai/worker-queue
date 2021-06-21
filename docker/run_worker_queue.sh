77aff36a5631bcdb9eceb296769ccff4e4d5b964
Otso Rasimus <orasimus@gmail.com>
2021-06-21 14:30:54 +0300

    Run cron to reconcile the SSL certificates

--- docker/run_worker_queue.sh
+++ docker/run_worker_queue.sh
@@ -14,6 +14,10 @@ if test ! -d /var/lib/acme/live/"$QUEUE_ADDRESS"; then
     acmetool want "$QUEUE_ADDRESS"
 fi
 
+# acmetool has a cronjob to reconcile the SSL certificates,
+# so we start the cron daemon
+/usr/sbin/cron
+
 sed -i "s|# requirepass foobared|requirepass $REDIS_PASSWORD|" /etc/redis/redis.conf
 sed -i "s|FILL_ADDRESS|$QUEUE_ADDRESS|" /etc/redis/redis.conf
 
