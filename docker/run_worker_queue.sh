3a87fdd1bd73f46c8c98a0b307dc01b68911ed4c
Drazen Dodik <drazend@live.fi>
2022-03-23 14:08:34 +0200

    update docker setup

--- docker/run_worker_queue.sh
+++ docker/run_worker_queue.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 
-if [ -z "$QUEUE_ADDRESS" ]; then
-    >&2 echo "QUEUE_ADDRESS not set"
-    exit 1
-fi
-
 if [ -z "$REDIS_PASSWORD" ]; then
     >&2 echo "REDIS_PASSWORD not set"
     exit 1
 fi
 
 # Make bash more strict about errors
-set -euo pipefail
+#set -euo pipefail
 
-if test ! -d /var/lib/acme/live/"$QUEUE_ADDRESS"; then
-    acmetool want "$QUEUE_ADDRESS"
+if [ -z "$QUEUE_ADDRESS" ]; then
+    echo "QUEUE_ADDRESS not. Skipping self-signed certificate"
+
+    sed -i "/FILL_ADDRESS/s/^/# /" /etc/redis/redis.conf
+    sed -i '/^[^#]/ s/\(^.*tls-port 63790.*$\)/#\ \1/' /etc/redis/redis.conf
+    sed -i '/^[^#]/ s/\(^.*tls-auth-clients.*$\)/#\ \1/' /etc/redis/redis.conf
+    sed -i "s|port 0|port 6379|" /etc/redis/redis.conf
+else
+    if test ! -d /var/lib/acme/live/"$QUEUE_ADDRESS"; then
+        acmetool want "$QUEUE_ADDRESS"
+    fi
+
+    # acmetool has a cronjob to reconcile the SSL certificates,
+    # so we start the cron daemon
+    /usr/sbin/cron
+
+    sed -i "s|FILL_ADDRESS|$QUEUE_ADDRESS|" /etc/redis/redis.conf
+    sed -i "s|# tls-port 63790|tls-port $REDIS_PORT|" /etc/redis/redis.conf
 fi
 
-# acmetool has a cronjob to reconcile the SSL certificates,
-# so we start the cron daemon
-/usr/sbin/cron
-
 sed -i "s|# requirepass foobared|requirepass $REDIS_PASSWORD|" /etc/redis/redis.conf
-sed -i "s|FILL_ADDRESS|$QUEUE_ADDRESS|" /etc/redis/redis.conf
 
 exec redis-server /etc/redis/redis.conf
